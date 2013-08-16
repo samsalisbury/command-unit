8406b86e20ca2a85bfcf23f6c428e3b144d68c02
Sam Salisbury <samsalisbury@gmail.com>
2013-08-16 23:48:38 +0100

    Moved test code out of lib

--- lib/command-unit.rb
+++ lib/command-unit.rb
@@ -86,27 +86,3 @@ module CommandUnit
   end
 
 end
-
-include CommandUnit
-
-scenario 'When blahblahs are whatnots' do
-
-  set_up do |context|
-    puts 'set_up called!'
-    context[:thing] = 'some property set in set_up'
-  end
-
-  when_i "don't do anything" do |context|
-    # Doing nothing
-    puts 'when_i called!'
-    context[:thing2] = 'thing'
-  end
-
-  i_expect 'to see a nice success message' do |context|
-    puts 'i_expect called!'
-    context[:thing3] = 'thang'
-  end
-
-end
-
-run
